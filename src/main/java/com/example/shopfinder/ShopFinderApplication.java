4c129af789852b814721044abfec0d3b744efd78
ofadeyi <ofadeyi@gmail.com>
2016-10-25 05:43:34 +0100

    init

---
+++ src/main/java/com/example/shopfinder/ShopFinderApplication.java
@@ -0,0 +1,12 @@
+package com.example.shopfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopFinderApplication.class, args);
+	}
+}
